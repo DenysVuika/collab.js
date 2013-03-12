cc2dfe7c94ae6691c32735ebed2cdd3cc52f2268
Denis Vuyka <denis.vuyka@gmail.com>
2013-03-12 20:44:55 +0200

    OpenShift db configuration sample; refs #8

--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,25 @@
 var config = require('./config.global');
 
-// TODO: override global settings here
+// OVERRIDE GLOBAL SETTINGS HERE
 
-// configure data provider
+// DATA PROVIDER CONFIGURATION SAMPLES
+
+// SAMPLE #1: Local database
+/*
 config.data.provider = 'collabjs.data.mysql';
 config.data.host = 'localhost';
 config.data.database = 'collabjs';
 config.data.user = '<user>';
 config.data.password = '<password>';
+*/
+
+// SAMPLE #2: Red Hat OpenShift Configuration (MySQL cartridge)
+/*
+config.data.provider = 'collabjs.data.mysql';
+config.data.host = process.env.OPENSHIFT_MYSQL_DB_HOST;
+config.data.database = 'collabjs';
+config.data.user = process.env.OPENSHIFT_MYSQL_DB_USERNAME;
+config.data.password = process.env.OPENSHIFT_MYSQL_DB_PASSWORD;
+*/
 
 module.exports = config;
