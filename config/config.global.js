0e357450911cc9cb7345ffea8c21585f09a46275
Denis Vuyka <denis.vuyka@gmail.com>
2013-06-20 09:58:23 +0300

    improved global config readability

--- config/config.global.js
+++ config/config.global.js
@@ -1,66 +1,70 @@
 var config = module.exports = {};
 
-config.env = {};
-config.env.port = process.env.PORT || 3000;
-config.env.ipaddress = '0.0.0.0';
-// used to generate 'back' urls in emails, 
-// helps solving issues with load balancers and proxies
-config.hostname = 'http://localhost:3000';
-
-// simple invitation code
-config.invitation = {};
-config.invitation.enabled = false;
-config.invitation.code = '123123123';
-
-// data layer
-config.data = {};
-config.data.provider = 'collabjs.data.mysql';
-//config.data.provider = 'collabjs.data.mssql';
-// Note: if defined, 'connectionString' value supersedes parameters host/database/user/password
-// this allows providing driver-specific connection strings like one the following samples:
-//  'Driver={SQL Server Native Client 11.0};Server={.};Database={collabjs};Trusted_Connection={Yes};'
-//  'mysql://user:pass@host/db?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700'
-config.data.connectionString = null;
-config.data.host = 'localhost';
-config.data.database = 'collabjs';
-config.data.user = '<username>';
-config.data.password = '<password>';
-
-// mail settings
-config.smtp = {};
-config.smtp.enabled = false;
-config.smtp.host = '<host>';
-config.smtp.user = '<user>';
-config.smtp.password = '<password>';
-// used for automatic notification purposes
-config.smtp.noreply = 'collab.js <noreply@collabjs.org>';
-
-// recaptcha settings
-config.recaptcha = {};
-config.recaptcha.enabled = false;
-config.recaptcha.publicKey = '6LeWatcSAAAAABpe4Xkm1-fKKOnCG3hF0Z_pfwQ1'; // localhost
-config.recaptcha.privateKey = '6LeWatcSAAAAAE6MsdnnV6gA8fzSoUmmPHG8ZSLq'; // localhost
-
-// ui settings
-config.ui = {};
-config.ui.brand = 'collab.js';
-config.ui.copyright = '2013 Denys Vuika';
-
-config.ui.sidebar = {
-  // custom sidebar categories with number or links per each
-	categories: [],
-  // links for special 'Administration' category
-  // available for users assigned to 'administrator' role
-  administration: []
+config.env = {
+  port: process.env.PORT || 3000,
+  ipaddress: '0.0.0.0',
+  // used by web sockets and to generate 'back' urls in emails,
+  // helps solving issues with load balancers and proxies
+  hostname: 'http://localhost:3000'
 };
 
-config.ui.header = {
-	links: [
-	{
-		text: 'People',
-		url: '/people',
-    icon: 'icon-group'
-	}]
+// simple invitation code
+config.invitation = {
+  enabled: false,
+  code: '123123123'
+};
+
+// data layer
+config.data = {
+  provider: 'collabjs.data.mysql',
+  //provider: 'collabjs.data.mssql',
+  // Note: if defined, 'connectionString' value supersedes parameters host/database/user/password
+  // this allows providing driver-specific connection strings like one the following samples:
+  //  'Driver={SQL Server Native Client 11.0};Server={.};Database={collabjs};Trusted_Connection={Yes};'
+  //  'mysql://user:pass@host/db?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700'
+  connectionString: null,
+  host: 'localhost',
+  database: 'collabjs',
+  user: '[username]',
+  password: '[password]'
+};
+
+// mail settings
+config.smtp = {
+  enabled: false,
+  host: '[host]',
+  user: '[user]',
+  password: '[password]',
+  // used for automatic notification purposes
+  noreply: 'collab.js <noreply@collabjs.org>'
+};
+
+// recaptcha settings
+config.recaptcha = {
+  enabled: false,
+  publicKey: '6LeWatcSAAAAABpe4Xkm1-fKKOnCG3hF0Z_pfwQ1', // localhost
+  privateKey: '6LeWatcSAAAAAE6MsdnnV6gA8fzSoUmmPHG8ZSLq'
+};
+
+// ui settings
+config.ui = {
+  brand: 'collab.js',
+  copyright: '2013 Denys Vuika',
+  header: {
+    links: [
+      {
+        text: 'People',
+        url: '/people',
+        icon: 'icon-group'
+      }]
+  },
+  sidebar: {
+    // custom sidebar categories with number or links per each
+    categories: [],
+    // links for special 'Administration' category
+    // available for users assigned to 'administrator' role
+    administration: []
+  }
 };
 
 //
@@ -99,15 +103,4 @@ config.ui.sidebar.administration = [
     url: '/admin/sample2'
   }
 ];
-*/
-
-//
-// header sample
-/*
-config.ui.header.links = [
-{
-	text: 'People',
-	url: '/people',
-	icon: 'icon-group'
-}];
 */
