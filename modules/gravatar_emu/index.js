9f72bd06da531f9692c943d784d993a90fa597bd
Denis Vuyka <denis.vuyka@gmail.com>
2013-07-12 15:57:53 +0300

    added simple gravatar emulator module

---
+++ modules/gravatar_emu/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+  Simple gravatar emulator for development purposes.
+  Typically used for offline development and testing of collab.js timeline.
+  Please change your configuration settings in order to enable it:
+    config.env.avatarServer = 'http://127.0.0.1:3000';
+ */
+
+module.exports = function (context) {
+  var app = context.app;
+
+  app.get('/avatar/:id?', function (req, res) {
+    res.sendfile(__dirname + '/public/img/avatar.png');
+  });
+};
