3ae2c0b20b75540db9d8901e61400c095dbf8cb8
Denis Vuyka <denis.vuyka@gmail.com>
2013-07-25 13:12:28 +0300

    enabled proxy trust; added AWS elastic beanstalk configuration sample

--- config/config.default.js
+++ config/config.default.js
@@ -2,10 +2,8 @@ var config = require('./config.global');
 
 // OVERRIDE GLOBAL SETTINGS HERE
 
-//
-// SAMPLE #1: Local database
-//
 /*
+//  SAMPLE #1: Local deployment (MySQL)
 config.data.provider = 'collabjs.data.mysql';
 config.data.sessionStore = 'collabjs.session.mysql';
 config.data.host = 'localhost';
@@ -14,10 +12,8 @@ config.data.user = '<user>';
 config.data.password = '<password>';
 */
 
-//
-// SAMPLE #2: Red Hat OpenShift Configuration (with MySQL cartridge)
-//
 /*
+//  SAMPLE #2: Red Hat OpenShift Configuration (with MySQL cartridge)
 config.env.ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 config.env.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 config.data.provider = 'collabjs.data.mysql';
@@ -26,6 +22,18 @@ config.data.host = process.env.OPENSHIFT_MYSQL_DB_HOST;
 config.data.database = 'collabjs';
 config.data.user = process.env.OPENSHIFT_MYSQL_DB_USERNAME;
 config.data.password = process.env.OPENSHIFT_MYSQL_DB_PASSWORD;
+*/
+
+/*
+//  SAMPLE #3: AWS Elastic Beanstalk (with MySQL RDS)
+config.env.ipaddress = process.env.COLLABJS_NODEJS_IP || '127.0.0.1';
+config.env.port = process.env.PORT || 3000;
+config.data.provider = 'collabjs.data.mysql';
+config.data.sessionStore = 'collabjs.session.mysql';
+config.data.host = process.env.RDS_HOSTNAME;
+config.data.database = 'collabjs';
+config.data.user = process.env.RDS_USERNAME;
+config.data.password = process.env.RDS_PASSWORD;
 */
 
 module.exports = config;
