2582ba499e71746ffdb74931c8eb20998eb3baef
Denis Vuyka <denis.vuyka@gmail.com>
2013-08-27 17:05:50 +0300

    got rid of 'bootstrap form helpers' library; closes #80

--- public/js/collabjs.js
+++ public/js/collabjs.js
@@ -30,6 +30,227 @@ var collabjs = collabjs || {
   }
 };
 
+collabjs.countries = {
+  'AF': 'Afghanistan',
+  'AL': 'Albania',
+  'DZ': 'Algeria',
+  'AS': 'American Samoa',
+  'AD': 'Andorra',
+  'AO': 'Angola',
+  'AI': 'Anguilla',
+  'AQ': 'Antarctica'  ,
+  'AG': 'Antigua and Barbuda' ,
+  'AR': 'Argentina' ,
+  'AM': 'Armenia' ,
+  'AW': 'Aruba' ,
+  'AU': 'Australia' ,
+  'AT': 'Austria' ,
+  'AZ': 'Azerbaijan'  ,
+  'BH': 'Bahrain' ,
+  'BD': 'Bangladesh',
+  'BB': 'Barbados'  ,
+  'BY': 'Belarus',
+  'BE': 'Belgium',
+  'BZ': 'Belize',
+  'BJ': 'Benin',
+  'BM': 'Bermuda',
+  'BT': 'Bhutan',
+  'BO': 'Bolivia',
+  'BA': 'Bosnia and Herzegovina',
+  'BW': 'Botswana',
+  'BR': 'Brazil',
+  'VG': 'British Virgin Islands',
+  'BN': 'Brunei',
+  'BG': 'Bulgaria',
+  'BF': 'Burkina Faso',
+  'BI': 'Burundi',
+  'CI': 'Côte d\'Ivoire',
+  'KH': 'Cambodia',
+  'CM': 'Cameroon',
+  'CA': 'Canada',
+  'CV': 'Cape Verde',
+  'KY': 'Cayman Islands',
+  'CF': 'Central African Republic',
+  'TD': 'Chad',
+  'CL': 'Chile',
+  'CN': 'China',
+  'CO': 'Colombia',
+  'KM': 'Comoros',
+  'CG': 'Congo',
+  'CR': 'Costa Rica',
+  'HR': 'Croatia',
+  'CU': 'Cuba',
+  'CY': 'Cyprus',
+  'CZ': 'Czech Republic',
+  'CD': 'Democratic Republic of the Congo',
+  'DK': 'Denmark',
+  'DJ': 'Djibouti',
+  'DM': 'Dominica',
+  'DO': 'Dominican Republic',
+  'TP': 'East Timor',
+  'EC': 'Ecuador',
+  'EG': 'Egypt',
+  'SV': 'El Salvador',
+  'GQ': 'Equatorial Guinea',
+  'ER': 'Eritrea',
+  'EE': 'Estonia',
+  'ET': 'Ethiopia',
+  'FO': 'Faeroe Islands',
+  'FK': 'Falkland Islands',
+  'FJ': 'Fiji',
+  'FI': 'Finland',
+  'MK': 'Former Yugoslav Republic of Macedonia',
+  'FR': 'France',
+  'GA': 'Gabon',
+  'GE': 'Georgia',
+  'DE': 'Germany',
+  'GH': 'Ghana',
+  'GR': 'Greece',
+  'GL': 'Greenland',
+  'GD': 'Grenada',
+  'GU': 'Guam',
+  'GT': 'Guatemala',
+  'GN': 'Guinea',
+  'GW': 'Guinea-Bissau',
+  'GY': 'Guyana',
+  'HT': 'Haiti',
+  'HN': 'Honduras',
+  'HK': 'Hong Kong',
+  'HU': 'Hungary',
+  'IS': 'Iceland',
+  'IN': 'India',
+  'ID': 'Indonesia',
+  'IR': 'Iran',
+  'IQ': 'Iraq',
+  'IE': 'Ireland',
+  'IL': 'Israel',
+  'IT': 'Italy',
+  'JM': 'Jamaica',
+  'JP': 'Japan',
+  'JO': 'Jordan',
+  'KZ': 'Kazakhstan',
+  'KE': 'Kenya',
+  'KI': 'Kiribati',
+  'KW': 'Kuwait',
+  'KG': 'Kyrgyzstan',
+  'LA': 'Laos',
+  'LV': 'Latvia',
+  'LB': 'Lebanon',
+  'LS': 'Lesotho',
+  'LR': 'Liberia',
+  'LY': 'Libya',
+  'LI': 'Liechtenstein',
+  'LT': 'Lithuania',
+  'LU': 'Luxembourg',
+  'MO': 'Macau',
+  'MG': 'Madagascar',
+  'MW': 'Malawi',
+  'MY': 'Malaysia',
+  'MV': 'Maldives',
+  'ML': 'Mali',
+  'MT': 'Malta',
+  'MH': 'Marshall Islands',
+  'MR': 'Mauritania',
+  'MU': 'Mauritius',
+  'MX': 'Mexico',
+  'FM': 'Micronesia',
+  'MD': 'Moldova',
+  'MC': 'Monaco',
+  'MN': 'Mongolia',
+  'ME': 'Montenegro',
+  'MS': 'Montserrat',
+  'MA': 'Morocco',
+  'MZ': 'Mozambique',
+  'MM': 'Myanmar',
+  'NA': 'Namibia',
+  'NR': 'Nauru',
+  'NP': 'Nepal',
+  'NL': 'Netherlands',
+  'AN': 'Netherlands Antilles',
+  'NZ': 'New Zealand',
+  'NI': 'Nicaragua',
+  'NE': 'Niger',
+  'NG': 'Nigeria',
+  'NF': 'Norfolk Island',
+  'KP': 'North Korea',
+  'MP': 'Northern Marianas',
+  'NO': 'Norway',
+  'OM': 'Oman',
+  'PK': 'Pakistan',
+  'PW': 'Palau',
+  'PA': 'Panama',
+  'PG': 'Papua New Guinea',
+  'PY': 'Paraguay',
+  'PE': 'Peru',
+  'PH': 'Philippines',
+  'PN': 'Pitcairn Islands',
+  'PL': 'Poland',
+  'PT': 'Portugal',
+  'PR': 'Puerto Rico',
+  'QA': 'Qatar',
+  'RO': 'Romania',
+  'RU': 'Russia',
+  'RW': 'Rwanda',
+  'ST': 'São Tomé and Príncipe',
+  'SH': 'Saint Helena',
+  'KN': 'Saint Kitts and Nevis',
+  'LC': 'Saint Lucia',
+  'VC': 'Saint Vincent and the Grenadines',
+  'WS': 'Samoa',
+  'SM': 'San Marino',
+  'SA': 'Saudi Arabia',
+  'SN': 'Senegal',
+  'RS': 'Serbia',
+  'SC': 'Seychelles',
+  'SL': 'Sierra Leone',
+  'SG': 'Singapore',
+  'SK': 'Slovakia',
+  'SI': 'Slovenia',
+  'SB': 'Solomon Islands',
+  'SO': 'Somalia',
+  'ZA': 'South Africa',
+  'GS': 'South Georgia and the South Sandwich Islands',
+  'KR': 'South Korea',
+  'ES': 'Spain',
+  'LK': 'Sri Lanka',
+  'SD': 'Sudan',
+  'SR': 'Suriname',
+  'SZ': 'Swaziland',
+  'SE': 'Sweden',
+  'CH': 'Switzerland',
+  'SY': 'Syria',
+  'TW': 'Taiwan',
+  'TJ': 'Tajikistan',
+  'TZ': 'Tanzania',
+  'TH': 'Thailand',
+  'BS': 'The Bahamas',
+  'GM': 'The Gambia',
+  'TG': 'Togo',
+  'TO': 'Tonga',
+  'TT': 'Trinidad and Tobago',
+  'TN': 'Tunisia',
+  'TR': 'Turkey',
+  'TM': 'Turkmenistan',
+  'TC': 'Turks and Caicos Islands',
+  'TV': 'Tuvalu',
+  'VI': 'US Virgin Islands',
+  'UG': 'Uganda',
+  'UA': 'Ukraine',
+  'AE': 'United Arab Emirates',
+  'GB': 'United Kingdom',
+  'US': 'United States',
+  'UY': 'Uruguay',
+  'UZ': 'Uzbekistan',
+  'VU': 'Vanuatu',
+  'VA': 'Vatican City',
+  'VE': 'Venezuela',
+  'VN': 'Vietnam',
+  'EH': 'Western Sahara',
+  'YE': 'Yemen',
+  'ZM': 'Zambia',
+  'ZW': 'Zimbabwe'
+};
+
 /*
   jQuery plugin: Twitter-like dynamic character countdown for textareas
 
@@ -109,11 +330,9 @@ $(function () {
       // show only when value is provided
       if (valueUnwrapped && valueUnwrapped.length > 0) {
         $element.css('display', 'block');
-        $element.attr('data-country', valueUnwrapped);
-        var data = $element.data();
-        delete data.bfhcountries;
-        data.country = valueUnwrapped;
-        $element.bfhcountries(data);
+        var content = '<i class="flag-icon-16 flag-' + valueUnwrapped.toLowerCase() + '"></i>';
+        content += collabjs.countries[valueUnwrapped.toUpperCase()];
+        $element.empty().html(content);
       } else {
         $element.css('display', 'none');
       }
@@ -782,6 +1001,45 @@ collabjs.ui.initAccountView = function () {
         $('#submit').attr('disabled', 'disabled');
       }
     });
+
+    /*
+    function getCountryData() {
+      var countryData = [];
+      for (var key in collabjs.countries) {
+        if (collabjs.countries.hasOwnProperty(key)) {
+          countryData.push({ id: key, text: collabjs.countries[key] });
+        }
+      }
+      return { results: countryData };
+    }
+    */
+
+    var countryData = [];
+    for (var key in collabjs.countries) {
+      if (collabjs.countries.hasOwnProperty(key)) {
+        countryData.push({ id: key, text: collabjs.countries[key] });
+      }
+    }
+
+    function formatCountry (entry) {
+      if (!entry.id) {
+        return entry.text;
+      }
+
+      return '<i class="flag-icon-16 flag-' + entry.id.toLowerCase() + '"></i>' + entry.text;
+    }
+
+    var locationValue = $('#location').data('location');
+
+    $('#location').select2({
+      placeholder: 'Select a Country',
+      allowClear: true,
+      //data: getCountryData,
+      data: countryData,
+      formatResult: formatCountry,
+      formatSelection: formatCountry
+    }).select2('val', locationValue);
+
   });
 };
 
