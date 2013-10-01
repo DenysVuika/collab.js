523c4b0568c7e33c8259f5f041db41a5581ce8c4
Denis Vuyka <denis.vuyka@gmail.com>
2013-10-01 20:33:53 +0300

    js minification; closes #89

--- gruntfile.js
+++ gruntfile.js
@@ -16,11 +16,27 @@ module.exports = function (grunt) {
         src: 'public/css/collabjs.css',
         dest: 'public/css/collabjs.min.css'
       }
+    },
+    uglify: {
+      options: {
+        mangle: false,
+        banner: '/*!\n' +
+          '* <%= pkg.name %> v<%= pkg.version %>\n' +
+          '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+          '* License: MIT\n' +
+          '* http://www.opensource.org/licenses/mit-license.php\n' +
+          '*/\n'
+      },
+      js: {
+        src: 'public/js/collabjs.js',
+        dest: 'public/js/collabjs.min.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['cssmin']);
+  grunt.registerTask('default', ['cssmin', 'uglify']);
 };
