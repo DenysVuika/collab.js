97aa6e58e38bfcf6701f5753f258df1fc3a1547d
Denis Vuyka <denis.vuyka@gmail.com>
2014-02-02 15:41:22 +0200

    Improved grunt workflow

--- gruntfile.js
+++ gruntfile.js
@@ -20,70 +20,46 @@ module.exports = function (grunt) {
     concat: {
       options: {
         banner: '/*!\n' +
-          '* <%= pkg.name %> v<%= pkg.version %>\n' +
-          '* Copyright (c) 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-          '* License: MIT\n' +
-          '* http://www.opensource.org/licenses/mit-license.php\n' +
-          '*/\n'
+                '* <%= pkg.name %> v<%= pkg.version %>\n' +
+                '* Copyright (c) 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                '* License: MIT\n' +
+                '* http://www.opensource.org/licenses/mit-license.php\n' +
+                '*/\n'
       },
-      "js-services": {
-        src: 'public/js/services/*Service.js',
-        dest: 'public/js/services.js'
-      },
-      "js-directives": {
-        src: 'public/js/directives/*.js',
-        dest: 'public/js/directives.js'
-      },
-      "js-filters": {
-        src: 'public/js/filters/*.js',
-        dest: 'public/js/filters.js'
-      },
-      "js-controllers" : {
-        src: 'public/js/controllers/*Controller.js',
-        dest: 'public/js/controllers.js'
+      "js-core" : {
+        src: [
+          'public/js/collabjs.js',
+          'public/js/app.js',
+          'public/js/services/*Service.js',
+          'public/js/directives/*.js',
+          'public/js/filters/*.js',
+          'public/js/controllers/*Controller.js'],
+        dest: 'temp/collabjs.core.js'
       }
     },
     uglify: {
       options: {
         mangle: false,
         banner: '/*!\n' +
-          '* <%= pkg.name %> v<%= pkg.version %>\n' +
-          '* Copyright (c) 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-          '* License: MIT\n' +
-          '* http://www.opensource.org/licenses/mit-license.php\n' +
-          '*/\n'
+                '* <%= pkg.name %> v<%= pkg.version %>\n' +
+                '* Copyright (c) 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                '* License: MIT\n' +
+                '* http://www.opensource.org/licenses/mit-license.php\n' +
+                '*/\n'
       },
-      "js-main": {
-        src: 'public/js/collabjs.js',
-        dest: 'public/js/collabjs.min.js'
-      },
-      "ja-app": {
-        src: 'public/js/app.js',
-        dest: 'public/js/app.min.js'
-      },
-      "js-services": {
-        src: 'public/js/services.js',
-        dest: 'public/js/services.min.js'
-      },
-      "js-directives": {
-        src: 'public/js/directives.js',
-        dest: 'public/js/directives.min.js'
-      },
-      "js-filters": {
-        src: 'public/js/filters.js',
-        dest: 'public/js/filters.min.js'
-      },
-      "js-controllers": {
-        src: 'public/js/controllers.js',
-        dest: 'public/js/controllers.min.js'
+      "js-core": {
+        src: 'temp/collabjs.core.js',
+        dest: 'public/js/collabjs.core.min.js'
       }
-    }
+    },
+    clean : ['temp']
   });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task(s).
-  grunt.registerTask('default', ['cssmin', 'concat', 'uglify']);
+  grunt.registerTask('default', ['cssmin', 'concat', 'uglify', 'clean']);
 };
