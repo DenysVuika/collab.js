60d0c996fffdd9860507f642a4958b8d1ba0ff5f
Denis Vuyka <denis.vuyka@gmail.com>
2014-04-13 17:53:49 +0300

    unit tests for ExploreController

--- public/js/controllers/ExploreController.js
+++ public/js/controllers/ExploreController.js
@@ -1,20 +1,29 @@
 angular.module('collabjs.controllers')
-  .controller('ExploreController', ['$scope', '$timeout', '$http', '$routeParams', 'postsService',
-    function ($scope, $timeout, $http, $routeParams, postsService) {
+  .controller('ExploreController', ['$scope', '$routeParams', 'postsService',
+    function ($scope, $routeParams, postsService) {
       'use strict';
 
-      $scope.tag = $routeParams.tag;
       $scope.posts = [];
       $scope.isLoadingMorePosts = false;
 
-      if ($scope.tag) {
-        $scope.isLoadingMorePosts = true;
-        postsService.getPostsByTag($scope.tag).then(
-          function (data) {
-            $scope.posts = data || [];
-            $scope.isLoadingMorePosts = false;
-          }
-        );
-      }
+      $scope.init = function () {
+        $scope.tag = $routeParams.tag;
+        if ($scope.tag) {
+          $scope.isLoadingMorePosts = true;
+          postsService.getPostsByTag($scope.tag).then(
+            // success handler
+            function (data) {
+              $scope.posts = data || [];
+              $scope.isLoadingMorePosts = false;
+              // TODO: display some info in case no content was found
+            },
+            // error handler
+            function () {
+              // TODO: display error
+              $scope.isLoadingMorePosts = false;
+            }
+          );
+        }
+      };
     }
   ]);
