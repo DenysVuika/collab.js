f1b02ab3066d43dde3aa52f95e4c854bf039e374
Denis Vuyka <denis.vuyka@gmail.com>
2014-04-18 10:41:08 +0300

    unit tests for AccountService

--- public/js/services/AccountService.js
+++ public/js/services/AccountService.js
@@ -3,39 +3,62 @@ angular.module('collabjs.services')
     function ($http, $q) {
       'use strict';
       return {
+        /**
+         * Create new account.
+         * @param {string} account Account name.
+         * @param {string} name User name.
+         * @param {string} email Email address.
+         * @param {string} password Account password.
+         * @returns {promise}
+         */
         createAccount: function (account, name, email, password) {
           var d = $q.defer()
             , data = { account: account, name: name, email: email, password: password };
           $http.post('/api/account/register', data)
             .success(function (res) { d.resolve(res); })
-            .error(function (res) { d.reject(res); });
+            .error(function (err) { d.reject(err); });
           return d.promise;
         },
+        /**
+         * Get account details for currently logged in user.
+         * @returns {promise}
+         */
         getAccount: function () {
           var d = $q.defer();
           $http.get('/api/profile')
-            .success(function (data) { d.resolve(data); });
+            .success(function (res) { d.resolve(res); });
           return d.promise;
         },
-        updateAccount: function (data) {
+        /**
+         * Update account details of currently logged in user.
+         * @param json Settings in JSON format.
+         * @returns {promise}
+         */
+        updateAccount: function (json) {
           var d = $q.defer();
-          $http.post('/api/profile', data)
+          $http.post('/api/profile', json)
             .success(function () { d.resolve(true); });
           return d.promise;
         },
         // TODO: should receive 'old' and 'new' values
-        changePassword: function (data) {
+        changePassword: function (json) {
           var d = $q.defer();
-          $http.post('/api/profile/password', data)
+          $http.post('/api/profile/password', json)
             .success(function (res) { d.resolve(res); })
-            .error(function (data) { d.reject(data); });
+            .error(function (err) { d.reject(err); });
           return d.promise;
         },
+        /**
+         * Changes email address for currently logged in user.
+         * @param oldValue Old email.
+         * @param newValue New email.
+         * @returns {promise}
+         */
         changeEmail: function (oldValue, newValue) {
           var d = $q.defer();
           $http.post('/api/profile/email', { oldValue: oldValue, newValue: newValue })
             .success(function (res) { d.resolve(res); })
-            .error(function (data) { d.reject(data); });
+            .error(function (err) { d.reject(err); });
           return d.promise;
         }
       };
