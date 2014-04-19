75870872720ddb2d115fb2d00bd2da7b2793d075
Denis Vuyka <denis.vuyka@gmail.com>
2014-04-19 10:57:06 +0300

    unit tests for HelpService

--- public/js/services/HelpService.js
+++ public/js/services/HelpService.js
@@ -2,6 +2,11 @@ angular.module('collabjs.services')
   .service('helpService', ['$http', '$q', function ($http, $q) {
     'use strict';
     return {
+      /**
+       * Get article content by article name
+       * @param {string} name Article name.
+       * @returns {promise}
+       */
       getArticle: function (name) {
         var d = $q.defer();
         var query = (name) ? '/api/help/' + name : '/api/help';
